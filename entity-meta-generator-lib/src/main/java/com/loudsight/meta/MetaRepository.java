2ac14059ea7dec12245d6e89ac82d5612231ffaa
munyengm <munyengm@users.noreply.github.com>
2025-10-23 14:55:06 -0400

    Automatically register Meta upon first access

--- entity-meta-generator-lib/src/main/java/com/loudsight/meta/MetaRepository.java
+++ entity-meta-generator-lib/src/main/java/com/loudsight/meta/MetaRepository.java
@@ -1,19 +1,19 @@
 package com.loudsight.meta;
 
+import com.loudsight.helper.ClassHelper;
+
 import java.util.Collection;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Objects;
 
 public class MetaRepository {
-        private static class MetaRepositoryHolder {
-            private static final MetaRepository INSTANCE = new MetaRepository();
-        }
-        // global access point
-        public static MetaRepository getInstance() {
-            return MetaRepositoryHolder.INSTANCE;
-        }
-    private MetaRepository() {}
+    private static class MetaRepositoryHolder {
+        private static final MetaRepository INSTANCE = new MetaRepository();
+    }
+    public static MetaRepository getInstance() {
+        return MetaRepositoryHolder.INSTANCE;
+    }
 
     private final Map<String, Meta<?>> metaByTypeName = new HashMap<>();
     private final Map<Class<?>, Meta<?>> metaByClass = new HashMap<>();
@@ -23,16 +23,48 @@ public class MetaRepository {
         metaByClass.put(meta.getTypeClass(), meta);
     }
 
-    public  <T> Meta<T> getMeta(Class<T> aClass) {
-        return (Meta<T>)metaByClass.get(aClass);
+    private static <T> Meta<T> loadMeta(Class<T> someClass) {
+        return loadMeta(someClass.getName());
+    }
+
+    private static <T> Meta<T> loadMeta(String someClassName) {
+        try {
+            String metaClassName = someClassName + "Meta";
+            Class<?> metaClass = Class.forName(metaClassName);
+            return ClassHelper.uncheckedCast(metaClass.getMethod("getInstance").invoke(null));
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public <T> Meta<T> getMeta(Class<T> aClass) {
+        try {
+            return ClassHelper.uncheckedCast(metaByClass.computeIfAbsent(aClass, it -> {
+                var meta = loadMeta(aClass);
+                metaByTypeName.put(aClass.getName(), meta);
+                return meta;
+            }));
+        } catch (Exception e) {
+            return null;
+        }
     }
 
     public <T> Meta<T> getMeta(String typeName) {
-        return (Meta<T>)metaByTypeName.get(typeName);
+        try {
+            return ClassHelper.uncheckedCast(metaByTypeName.computeIfAbsent(typeName, it -> {
+                var meta = loadMeta(typeName);
+                metaByClass.put(meta.getTypeClass(), meta);
+                register(meta);
+                return meta;
+            }));
+        } catch (Exception e) {
+            return null;
+        }
     }
 
     public Map<String, ?> toMap(Object entity) {
 //        val meta = getMeta(entity::class)
+
 //        if (meta == null) {
 //            throw IllegalArgumentException("No meta found for entity: $entity")
 //        }
@@ -47,7 +79,7 @@ public class MetaRepository {
 //                if (value != null) {
 //                    entityMap.put(it.name, value)
 //                }
-            }
+        }
         return entityMap;
     }
 }
